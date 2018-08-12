94058690a5e9e5d40a811298104aee6e35cd5de8
George <georgesu1074@berkeley.edu>
2018-08-12 03:14:33 +0000

    Updated Mongoose to Connect to Environment DatabaseURL

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var express =	require("express"), // import Express package
     request =		require("request"), // Request package for making HTTP requests for API calls
     methodOverride = require("method-override"), // Method-Override package for PUT/DELETE route method over-rides
     mongoose =	require("mongoose"); // Mongoose package for interacting with MongoDB
+    
 // Connect Mongoose Server to DB; use new syntax (mongoose.connect("mongodb://localhost:27017/app_name", { useNewUrlParser: true });) if Mongo Version 5.2.1 or higher
-mongoose.connect("mongodb://localhost/elections"); // connects mongoose to database server; replace database_name with app’s database; if database_name doesn’t exist, creates the database
+mongoose.connect(process.env.DATABASEURL); // connects mongoose to database server; replace database_name with app’s database; if database_name doesn’t exist, creates the database
+// mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds219672.mlab.com:19672/shitty-voting-app");
 
 // BodyParser + Method-Override Boilerplate
 app.use(bodyParser.urlencoded({extended: true})); // Boilerplate for using BodyParser
@@ -114,15 +116,6 @@ app.get("/elections/:id/results", function(req, res) {
                     electionResults[nomination.name] += ballot.ordering.get(nomination._id.toString());
                 })
             })
-            // console.log('electionResults');
-            // console.log(electionResults);
-            // Election.findByIdAndUpdate(req.params.id, {results: electionResults}, function(err, currentElection) {
-            //     if (err) {
-            //         console.log(err);
-            //     } else {
-            //         res.render("results", {election: currentElection});
-            //     }
-            // })
             res.render("results", {results: electionResults});
         }
     }
